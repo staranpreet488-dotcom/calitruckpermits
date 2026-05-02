581895fc14edb7285897daf7bde7abe5522f3b62
taranpreet singh <taranpreetsingh@cqlsys.co.uk>
2026-05-02 22:16:11 +0530

    pdf

--- controller/Admin/DriverController.js
+++ controller/Admin/DriverController.js
@@ -8,7 +8,7 @@ const LinkModel = require("../../Model/LinkModel");
 const pdfmodel =require("../../Model/allpdf")
 
 require('dotenv').config();
-const BASE_URL = process.env.LiveBase_url || 'http://localhost:2000';
+const BASE_URL = process.env.LIVE_BASE_URL || 'http://localhost:2000';
 
 const helper = require("../../utility/helper");
 const helpers = require("../../utility/helpers");
